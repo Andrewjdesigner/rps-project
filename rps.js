dc717361fd07d289164ef0d5a2fc89e02493af19
Andrewjdesigner <andrew.johnson0993@gmail.com>
2023-09-02 00:27:37 -0400

    Add play round function. Incomplete!

--- rps.js
+++ rps.js
@@ -2,5 +2,17 @@ function getComputerChoice() {
     const rps = ["Rock", "Paper", "Scissors"];
 
     return rps[Math.floor(Math.random() * 3)];
+}
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            return "Tie game! Try again.";
+        } else if (computerSelection === "Scissors") {
+            return "Congratulations! You win!";
+        } else {
+            return "You lose. Try again!";
+        }
+        
+    }
 }
