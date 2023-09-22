09196e8d23ba46c5383dc6d7a65e2094b50f0474
Andrewjdesigner <andrew.johnson0993@gmail.com>
2023-09-21 21:16:36 -0400

    Disable buttons once one player reaches 3 score

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const btns = document.querySelectorAll("button");
 
 btns.forEach(btn => btn.addEventListener("click", () => {
@@ -15,40 +18,41 @@ function getComputerChoice() {
     return rps[Math.floor(Math.random() * 3)];
 }
 
-function game(playerChoice, computerChoice) {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-}
-
 function playRound(playerSelection, computerSelection) {
 
+    if (playerScore < 3 && computerScore < 3) {
+        const playerChoiceDisplay = document.querySelector("#player-choice");
+        playerChoiceDisplay.textContent = playerSelection;
 
-    const playerChoiceDisplay = document.querySelector("#player-choice");
-    playerChoiceDisplay.textContent = playerSelection;
+        const compChoiceDisplay = document.querySelector("#computer-choice");
+        compChoiceDisplay.textContent = computerSelection;
 
-    const compChoiceDisplay = document.querySelector("#computer-choice");
-    compChoiceDisplay.textContent = computerSelection;
-
-    if (playerSelection === computerSelection) {
-        tieGame();
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors"
-            || playerSelection === "Paper" && computerSelection === "Rock"
-            || playerSelection === "Scissors" && computerSelection === "Paper") {
-                playerWin();
+        if (playerSelection === computerSelection) {
+            tieGame();
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors"
+                || playerSelection === "Paper" && computerSelection === "Rock"
+                || playerSelection === "Scissors" && computerSelection === "Paper") {
+                    playerWin();
+        } else {
+            computerWin();
+        } 
     } else {
-        computerWin();
+        btns.forEach(btn => btn.setAttribute("disabled", true));
     }
 }
 
 function playerWin() {
     const winnerDisplay = document.querySelector("#round-winner");
     winnerDisplay.textContent = "Player Wins!";
+    playerScore++;
+    console.log("Player: " + playerScore);
 }
 
 function computerWin() {
     const winnerDisplay = document.querySelector("#round-winner");
     winnerDisplay.textContent = "Computer Wins!";
+    computerScore++;
+    console.log("Computer: " + computerScore);
 }
 
 function tieGame() {
